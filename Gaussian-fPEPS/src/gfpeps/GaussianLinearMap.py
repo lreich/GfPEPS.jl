e57d2b0ef9294478998168cef119a05d26d87465
Lukas Reich <lukasreich@online.de>
2025-08-20 22:53:47 +0200

    example

---
+++ Gaussian-fPEPS/src/gfpeps/GaussianLinearMap.py
@@ -0,0 +1,8 @@
+import jax.numpy as jnp
+from jax.scipy.linalg import inv
+from .ABD import getABD
+
+def GaussianLinearMap(Glocal,Gin):
+    r""" Glocal: Local project, Gin, Gamma in"""
+    A,B,D = getABD(Glocal)
+    return A + B @ inv(D+Gin) @ jnp.transpose(B) # for newer jax version.
