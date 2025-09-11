bccd2e1c2e99196ff1446c5a2aa08b40d34ecb71
Lukas Reich <lukasreich@online.de>
2025-09-11 11:24:23 +0200

    update

--- test/test_energy_BCS.jl
+++ test/test_energy_BCS.jl
@@ -3,23 +3,25 @@ using Test
 using GfPEPS
 using JSON: parsefile
 
-@testset "d_wave" begin
+@testset "Energy BCS" begin
+    @testset "d_wave" begin
     res = Gaussian_fPEPS(conf = parsefile(joinpath(GfPEPS.test_config_path, "conf_test_BCS_d_wave.json")))
     @test res.optim_res ≈ res.exact_energy
+    end;
+
+    @testset "p_wave" begin
+        res = Gaussian_fPEPS(conf = parsefile(joinpath(GfPEPS.test_config_path, "conf_test_BCS_p_wave.json")))
+        @test res.optim_res ≈ res.exact_energy
+    end;
+
+    @testset "s_wave" begin
+        res = Gaussian_fPEPS(conf = parsefile(joinpath(GfPEPS.test_config_path, "conf_test_BCS_s_wave.json")))
+        @test res.optim_res ≈ res.exact_energy
+    end;
 end;
 
-@testset "p_wave" begin
-    res = Gaussian_fPEPS(conf = parsefile(joinpath(GfPEPS.test_config_path, "conf_test_BCS_p_wave.json")))
-    @test res.optim_res ≈ res.exact_energy
-end;
 
-@testset "s_wave" begin
-    res = Gaussian_fPEPS(conf = parsefile(joinpath(GfPEPS.test_config_path, "conf_test_BCS_s_wave.json")))
-    @test res.optim_res ≈ res.exact_energy
-end;
+# @time Gaussian_fPEPS(conf = parsefile(joinpath(GfPEPS.test_config_path, "conf_test_BCS_d_wave.json")));
 
-
-@time Gaussian_fPEPS(conf = parsefile(joinpath(GfPEPS.test_config_path, "conf_test_BCS_d_wave.json")));
-
-bz = BrillouinZone2D(4,4,(:APBC,:PBC))
-GfPEPS.G_in_Fourier(bz,2)
+# bz = BrillouinZone2D(4,4,(:APBC,:PBC))
+# GfPEPS.G_in_Fourier(bz,2)
