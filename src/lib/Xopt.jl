5ae593036433bc3ad6063c2be3ff288479debf9b
Lukas Reich <lukasreich@online.de>
2025-10-05 22:23:40 +0200

    typo fix

--- src/lib/Xopt.jl
+++ src/lib/Xopt.jl
@@ -58,7 +58,7 @@ function get_X_opt(Nf::Int, Nv::Int, t::Real, μ::Real, pairing_type::String, Δ
     # First, find a better initial guess for X by solving for smaller system sizes (see: 10.1103/PhysRevLett.129.206401) 
     @info "Finding better initial guess for X by solving smaller system sizes..."
     # res_init = Optim.optimize(loss_init, g_init!, X, Optim.LBFGS(;m=20,manifold=Optim.Stiefel()), Optim.Options(
-    res_init = Optim.optimize(loss, g!, X, Optim.BFGS(;manifold=Optim.Stiefel()), Optim.Options(
+    res_init = Optim.optimize(loss_init, g!, X, Optim.BFGS(;manifold=Optim.Stiefel()), Optim.Options(
         iterations = maxiter,
         g_tol = grad_tol,
         show_trace = verbose,
